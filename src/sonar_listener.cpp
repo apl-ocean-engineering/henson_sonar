d77f5bfe5634e36191801bd48db7cf54892e7464
tannerpoling <tannerpoling@gmail.com>
2021-03-02 16:35:10 -0800

    sonar listener working with Aug 1 2020 version of imaging_sonar_msgs

--- src/sonar_listener.cpp
+++ src/sonar_listener.cpp
@@ -6,10 +6,11 @@
 // #include <Eigen3/Eigen/Dense>
 
 
-void sonarCallback(const imaging_sonar_msgs::SonarImage msg) // TODO change this to imaging sonar message
+void sonarCallback(const imaging_sonar_msgs::SonarImage& msg) // TODO change this to imaging sonar message
 {
   // send imaging sonar message to hensonLib functions
   // rosbag uses imaging_sonar_msgs/SonarImage messages
+  printf("got a message!\n");
 
 
 }
@@ -21,7 +22,7 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("oculus/drawn_sonar", 1000, sonarCallback); // TODO change first arg to name of sonar topic
+  ros::Subscriber sub = n.subscribe("/oculus/sonar_image", 1000, sonarCallback); // TODO change first arg to name of sonar topic
 
   ros::spin();
 
